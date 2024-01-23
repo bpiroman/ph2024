5c1b9da50d6fce52f065d7911276a1d1b42bba6e
bpiroman <b.piromanski@holdfasteng.com>
2024-01-23 15:39:14 +0800

    *

---
+++ data.js
@@ -0,0 +1,122 @@
+const nodes = [
+    {
+        node: 0,
+        type: "flight",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Manila around 5pm</br>Stay over night in Manila, Filipino Dinner and shopping",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 1,
+        type: "day",
+        title: {
+            text: "Manila and travel to Dagupan",
+            date: "24th January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 2,
+        type: "day",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 3,
+        type: "day",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 3,
+        type: "day",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 4,
+        type: "day",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 5,
+        type: "day",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 6,
+        type: "day",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 7,
+        type: "day",
+        title: {
+            text: "Perth to Singapore - Singapore to Manila",
+            date: "23rd January 2024",
+            content: {
+                text: "Arrive in Phillipines around ",
+                img: "imgurl goes here"
+            }
+        }
+    },
+    {
+        node: 8,
+        type: "flight",
+        title: {
+            text: "Manila to Singapore - Singapore to Perth",
+            date: "1st February 2024",
+            content: {
+                text: "stuff goes here",
+                img: "imgurl goes here"
+            }
+        }
+    }
+]
---
+++ main.js
@@ -0,0 +1,58 @@
+// DOM Manipulation
+
+const htmlMap = `
+<header class="it-header">
+  <h1>Phillipines 2024</h1>
+</header>
+<div id="data" class="main-container">
+
+</div>
+`;
+
+function clearPage() {
+    console.log('clearPage');
+    $('#app').empty();
+}
+
+function nodeContainerButton() {
+    console.log("yoyoyo");
+    $(this).next().toggle();
+}
+
+function addContent(node_data) {
+    console.log(node_data);
+    node_data.forEach(element => {
+        console.log(element.title.text);
+        const nodeText = element.title.text;
+        const nodeDate = element.title.date;
+        const nodeContentText = element.title.content.text;
+        const nodeContentImg = element.title.content.img;
+        const textTemplate = `<div class="node-container"><h3>${nodeText}, ${nodeDate}</h3></div><div class="content-container">${nodeContentText}</div>`;
+        $('#data').append(textTemplate);
+    });
+    $('.node-container').on("click", nodeContainerButton);
+}
+
+function buildLayout(state) {
+    if (state === "itinerary") {
+        document.querySelector('#app').innerHTML = htmlMap;
+    }
+}
+
+function render(state, node_data) {
+    clearPage();
+    buildLayout(state.page);
+    addContent(node_data);
+  }
+
+// State Manipulation
+
+const initialState = { random: "ph2024", page: "itinerary" };
+
+function getState() {
+    return JSON.parse(localStorage.getItem("ph2024"));
+}
+
+// Initialise
+
+render(getState() || initialState, nodes);
